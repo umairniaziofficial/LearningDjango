970d23afadae301ca1e9f4fa0c7813b918ec7185
M Umair Khan (Nizzy Solutions) <129067233+umairniaziofficial@users.noreply.github.com>
2024-02-22 21:53:05 +0500

    Finished Templates Lesson and Figured Out How to Add Cool Styles and Bootstrap

--- firstProject/blog/urls.py
+++ firstProject/blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="Home"),
-    path("about/", views.about, name="About"),
+    path("", views.home, name="blog-home"),
+    path("about/", views.about, name="blog-about"),
 ]
--- firstProject/blog/views.py
+++ firstProject/blog/views.py
@@ -1,9 +1,26 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+posts = [
+    {
+        "author": "Umair",
+        "title": "Blog Post 1",
+        "content": "First Post By Umair (NizzyPedia)",
+        "data_posted": "Feburary 22nd, 2024",
+    },
+    {
+        "author": "Khan",
+        "title": "Blog Post 2",
+        "content": "Second Post By Khan (NizzyPedia)",
+        "data_posted": "Feburary 31st, 2024",
+    },
+]
 
 
 def home(request):
-    return HttpResponse("<h1>I'm home Page</h1>")
+    context = {"posts": posts}
+    return render(request, "blog/home.html", context)
 
 
 def about(request):
-    return HttpResponse("<h1>I'm About Page</h1>")
+    return render(request, "blog/about.html", {"title": "About"})
